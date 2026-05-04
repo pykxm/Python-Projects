d9718ca0f19a3e2dadfd0517ed28498b72219eab
pykxm <pykxmz@gmail.com>
2026-05-04 23:23:36 +0800

    Update basic_webhook.py

--- Py Discord Webhook/basic_webhook.py
+++ Py Discord Webhook/basic_webhook.py
@@ -1,1 +1,16 @@
 
+import requests
+
+Webhook_URL = ""
+Message = ""
+username = ""
+
+data = {
+  "content": Message,
+  "username": username
+}
+
+response = requests.post(Webhook_URL, json=data)
+
+print("Message sent:", Message)
+print("Status:", response.status_code)
